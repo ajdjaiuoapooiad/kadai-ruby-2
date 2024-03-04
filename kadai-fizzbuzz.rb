a81ff9fad2e08333ede50af2884fb745fba7ff26
taro.kirameki <taro.kirameki@techacademy.jp>
2024-03-04 09:44:32 +0000

    first commit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz(number)
+  if number%3==0 && number%5==0
+    return "FizzBuzz"
+  elsif number%5==0
+    return "Buzz"
+  elsif number%3==0 
+    return "Fizz"
+  else 
+    return number
+  end
+      
+end
+
+num_max = 100
+numbers = 1..num_max
+numbers.each do |number|
+  puts fizzbuzz(number)
+end
